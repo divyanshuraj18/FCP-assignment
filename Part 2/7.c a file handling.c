110e46946cc6f27e786b7d296a8e2977e1e706c3
divyanshuraj18 <151809624+divyanshuraj18@users.noreply.github.com>
2023-12-26 12:29:33 +0530

    Create 7.c a file handling.c

---
+++ Part 2/7.c a file handling.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+FILE *file;
+char data[100];
+// Open a file for appending
+file = fopen("example.txt", "a");
+if (file == NULL) {
+printf("Error opening the file.\n");
+return 1;
+}
+// Get data from the user
+printf("Enter data to append to the file:\n");
+fgets(data, sizeof(data), stdin);
+// Append data to the file
+fprintf(file, "%s", data);
+// Close the file
+fclose(file);
+printf("Data appended to the file successfully.\n");
+return 0;
+}
